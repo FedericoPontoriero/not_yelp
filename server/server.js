937f41ee4c50e5e27ced9e0e7200c3e5fb66c284
Federico <federicolucaspontoriero@gmail.com>
2022-06-06 18:40:57 -0300

    server & first route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/getRestaurants', (req, res) => {
+	console.log('getRestaurants');
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Server active in port: ${port}`);
