0574ee004bf99eac2416712e3a3c16f9abe08700
Federico <federicolucaspontoriero@gmail.com>
2022-06-08 16:27:03 -0300

    context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,29 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import UpdatePage from './routes/UpdatePage';
 import Home from './routes/Home';
 import RestaurantdetailPage from './routes/RestaurantdetailPage';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 const App = () => {
 	return (
-		<div className='container'>
-			<BrowserRouter>
-				<Routes>
-					<Route exact path='/' element={<Home />} />
-					<Route
-						exact
-						path='/restaurants/:id/updates'
-						element={<UpdatePage />}
-					/>
-					<Route
-						exact
-						path='/restaurants/:id'
-						element={<RestaurantdetailPage />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</div>
+		<RestaurantsContextProvider>
+			<div className='container'>
+				<BrowserRouter>
+					<Routes>
+						<Route exact path='/' element={<Home />} />
+						<Route
+							exact
+							path='/restaurants/:id/updates'
+							element={<UpdatePage />}
+						/>
+						<Route
+							exact
+							path='/restaurants/:id'
+							element={<RestaurantdetailPage />}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</RestaurantsContextProvider>
 	);
 };
 
