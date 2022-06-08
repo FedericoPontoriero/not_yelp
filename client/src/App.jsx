3c198d662c9592d4278b953448cce551f32a6b10
Federico <federicolucaspontoriero@gmail.com>
2022-06-08 04:13:41 -0300

    Home page components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import UpdatePage from './routes/UpdatePage';
+import Home from './routes/Home';
+import RestaurantdetailPage from './routes/RestaurantdetailPage';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route exact path='/restaurants/:id/updates' element={<UpdatePage />} />
+				<Route
+					exact
+					path='/restaurants/:id'
+					element={<RestaurantdetailPage />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
---
+++ client/src/routes/RestaurantdetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RestaurantdetailPage() {
+	return <div>RestaurantdetailPage</div>;
+}
+
+export default RestaurantdetailPage;
