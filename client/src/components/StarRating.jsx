5651d13d0efa181ab6f64b8fb774b71e62744c9b
Federico <federicolucaspontoriero@gmail.com>
2022-06-14 14:19:48 -0300

    ready for production

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function StarRating({ rating }) {
 	const stars = [];
-	for (let i = 0; i <= 5; i++) {
+	for (let i = 1; i <= 5; i++) {
 		if (i <= rating) {
 			stars.push(<i key={i} className='fas fa-star text-warning'></i>);
 		} else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
